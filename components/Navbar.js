30bacb4cdd343a405e3deecfb21b5fe74c395f6b
bangkit16 <bangkitcaniago@gmail.com>
2022-11-19 23:18:58 +0700

    fix input navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,27 +5,28 @@ import Logo from '../public/assets/svg/Logo.svg';
 const Navbar = () => {
     return (
         // <nav>
-        <div className="flex mx-2 my-3">
-            <div className="basis-1/2 items-center font-semibold flex space-x-2  [&>*>span]:text-slates-100">
+        <div className="flex mx-auto mb-3  drop-shadow-md  backdrop-blur-md bg-white">
+            <div className="basis-1/2 items-center font-semibold flex space-x-2  [&>*>span]:text-slates-100 ml-3">
                 <div className="flex flex-row items-center space-x-2 p-2">
-                    <Logo className="w-8" />
+                    <Logo className="w-10" />
                     <div className="text-logo">
-                        <span className='text-[30px] font-medium mt-1 tracking-wide '>PortoLy</span>
+                        <span className='text-[30px] font-medium  '>PortoLy</span>
                     </div>
                 </div>
-                <div className="px-10 relative ">
-                    <span className='underlineNav text-[20px] font-medium  tracking-wide '>Home</span>
+                <div className="px-10  ">
+                    <span className='underlineNav text-[20px] font-medium my-auto  '>Home</span>
                     {/* <div className='bg-[#B1B2FF] w-6 h-[3px] flex mx-auto rounded'></div> */}
                 </div>
         
                 <div className="px-10 ">
-                    <span className='text-[20px] font-medium  tracking-wide'>Explore</span>
+                    <span className='text-[20px] font-medium  '>Explore</span>
                 </div>
                 <div className="px-10 ">
-                    <span className='text-[20px] font-medium  tracking-wide'>Projects</span>
+                    <span className='text-[20px] font-medium  '>Projects</span>
                 </div>
+                
             </div>
-            <div className="basis-1/2 flex justify-end p-3">
+            <div className="basis-1/2 flex justify-end p-3 mr-3">
                 <div className="sign-up-btn">
                     <button className='bg-[#B1B2FF] rounded-full p-2 px-4 font-bold text-white' type="submit">Sign Up</button>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 
 export default function Home() {
   return (
-    <div className="">
-
+    <div className="h-10 w-[500px] rounded-full bg-gradient-to-r from-[#FFD1E4]  via-[#BEE1F1] to-[#8AFBD1] py-1 px-[5px]">
+        <input className="flex h-full w-full items-center justify-center rounded-full pl-4 focus:outline-none  focus:ring-[#9fccf1] focus:ring-2" placeholder="Search..."/>
     </div>
+    // {/* <div className=" w-[500px]  h-2 p-1 bg-gradient-to-r from-[#FFD1E4]  via-[#BEE1F1] to-[#8AFBD1] rounded-full">
+    //   <input className="w-full block rounded-full mx-0 h-auto p-2 relative" placeholder="Search..."/>
+    // </div> */}
+    
+    
   )
 }
